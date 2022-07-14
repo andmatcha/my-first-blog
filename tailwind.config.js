87f7fd54ab9e7025b5a72ec1b3317dddc714944e
andmatcha <jj.mcr185@gmail.com>
2022-07-14 21:49:40 +0900

    tailwind cssを導入

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    purge: ['./pages/blog/*.{js,ts,jsx,tsx}'],
+    content: [],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
