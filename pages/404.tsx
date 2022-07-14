0a43b6a52863199bdb8bed1a87cdef552e09cc1d
andmatcha <jj.mcr185@gmail.com>
2022-07-15 00:48:56 +0900

    404ページ修正

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,13 @@
+import Link from "../components/global/MyLink";
+
 const Custom404 = () => {
     return (
         <>
-            <h1>ページが見つかりません</h1>
+            <h1>ページが見つかりません。</h1>
+            <p>URLをご確認ください。</p>
+            <Link href={'/'}>
+                ブログトップ
+            </Link>
         </>
     );
 }
