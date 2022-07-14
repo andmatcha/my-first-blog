1b5077f2d2c1a210c2112d25d2dc8d619d2bb6b7
andmatcha <jj.mcr185@gmail.com>
2022-07-14 23:35:35 +0900

    やっぱりtailwindcssやめました

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,10 +5,10 @@ import Link from "next/link";
 export default function Home() {
     return (
         <div className="m-4">
-            <h1 className="text-red-600 text-xl">ポートフォリオサイト</h1>
+            <h1>ポートフォリオサイト</h1>
             <p>ただいま準備中...</p>
             <Link href={"/blog"}>
-                <a className=" text-blue-600 underline">ブログはこちら</a>
+                <a>ブログはこちら</a>
             </Link>
         </div>
     );
--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    mode: 'jit', // JIT (just-in-time) modeを適用
-    content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/.{js,ts,jsx,tsx}'], // ビルド生成時の未使用のスタイルを除外し、パフォーマンスを最適化
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-}
