750004b874bbc1e853bc1104256b5b58b74dd75d
andmatcha <jj.mcr185@gmail.com>
2022-07-16 04:08:53 +0900

    google fonts を一つずつにした

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,10 +9,21 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
+                    {/* google fonts */}
                     <link
-                        href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@600&family=Noto+Sans+JP:wght@100;300;400;500&family=Roboto+Mono:ital,wght@0,300;0,400;0,600;1,300;1,400;1,600&display=swap"
+                        href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,300;0,400;0,600;1,300;1,400;1,600&display=swap"
                         rel="stylesheet"
                     />
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500&display=swap"
+                        rel="stylesheet"
+                    />
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@600&display=swap"
+                        rel="stylesheet"
+                    />
+
+                    {/* favicon.ico */}
                     <link
                         rel="shortcut icon"
                         href="favicon.ico"
