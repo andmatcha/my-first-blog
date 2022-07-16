4efd5fed116554857e1cdf86bb77f81a87eb35db
andmatcha <jj.mcr185@gmail.com>
2022-07-17 00:31:29 +0900

    サイドバーを少し作った

--- components/parts/SideBar.tsx
+++ components/parts/SideBar.tsx
@@ -1,7 +1,12 @@
 import { css } from "@emotion/react";
+import SideProfile from "./SideProfile";
 
 const SideBar = () => {
-    return <div css={styles.wrapper}>aaaa</div>;
+    return (
+        <div css={styles.wrapper}>
+            <SideProfile />
+        </div>
+    );
 };
 
 export default SideBar;
@@ -11,5 +16,9 @@ const styles = {
         border-left: 1px solid #edebe4;
         width: 100%;
         height: 100%;
+        padding: 0 24px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
     `,
 };
