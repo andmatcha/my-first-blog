cbcbe2d0976db5a99b6fb74309de96452dd419eb
andmatcha <jj.mcr185@gmail.com>
2022-07-18 00:07:00 +0900

    やっぱりdb接続して認証しようとするとダメだった

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,9 +1,9 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
+// import { PrismaAdapter } from "@next-auth/prisma-adapter";
+// import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
 export default NextAuth({
     providers: [
