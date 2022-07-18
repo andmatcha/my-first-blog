0d401163e3f57e6498b2cc52c6e3f19f5c8a4d84
andmatcha <jj.mcr185@gmail.com>
2022-07-18 14:52:12 +0900

    mdファイルからレコードを作成できるようにした

---
+++ pages/api/posts/createPosts.tsx
@@ -0,0 +1,74 @@
+import type { NextApiHandler } from "next";
+import path from "path";
+import fs from "fs";
+import matter from "gray-matter";
+import prisma from "../../../lib/prisma";
+
+const postsDirectory: string = path.join(process.cwd(), "posts");
+
+// mdファイルからデータ取得
+const getPostsDataFromMd = () => {
+    const fileNames: string[] = fs.readdirSync(postsDirectory);
+    const allPostsData: {
+        published: boolean;
+        title: string;
+        thumbnail: string;
+        description: string;
+        body: string;
+    }[] = [];
+    fileNames.forEach((fileName) => {
+        // const postId = fileName.replace(/\.md$/, ""); // 拡張子を除いたファイル名
+
+        const fullPath = path.join(postsDirectory, fileName);
+        const fileContent = fs.readFileSync(fullPath, "utf-8");
+        const matterResult = matter(fileContent);
+        const postData: {
+            published: boolean;
+            title: string;
+            thumbnail: string;
+            description: string;
+            body: string;
+        } = {
+            published: matterResult.data.published,
+            title: matterResult.data.title,
+            thumbnail: matterResult.data.thumbnail,
+            description: matterResult.data.description,
+            body: matterResult.content,
+        };
+
+        allPostsData.push(postData);
+        return;
+    });
+    console.log(allPostsData);
+    return allPostsData;
+};
+
+const postHandler = async (req, res) => {
+    const allPostsData = getPostsDataFromMd();
+    const allPostsDataOnDB = await prisma.post
+        .createMany({
+            data: allPostsData,
+        })
+        .catch((err) => {
+            console.log(err);
+            return { error: "Failed to read posts" };
+        })
+        .finally(async () => {
+            await prisma.$disconnect();
+        });
+    console.dir(allPostsDataOnDB, { depth: null });
+
+    return;
+};
+
+const handler: NextApiHandler = (req, res) => {
+    switch (req.method) {
+        case "GET":
+            postHandler(req, res);
+            break;
+        default:
+            return res.status(405).json({ error: "Method not allowed." });
+    }
+};
+
+export default handler;
