0a9f1400d6d91bdaa7a0b2c53e6f6837990cc1f9
andmatcha <jj.mcr185@gmail.com>
2022-07-28 23:54:33 +0900

    記事を降順に並び替え

--- lib/posts/selectAllPosts.ts
+++ lib/posts/selectAllPosts.ts
@@ -7,6 +7,9 @@ const selectAllPosts = async () => {
             where: {
                 published: true,
             },
+            orderBy: {
+                updatedAt: 'desc'
+            }
         })
         .catch((e) => {
             throw e;
